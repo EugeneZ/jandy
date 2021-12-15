5dc4be726e27a800d822f97f4c57ae0502854db0
Eugene Zaretskiy <ezaretskiy@attentivemobile.com>
2021-12-14 22:30:04 -0500

    Update 00.mjs

--- src/00.mjs
+++ src/00.mjs
@@ -8,10 +8,43 @@
 //     rl.question("Next value: ", (answer)=>{ resolve(answer)});
 //   });
 
-let __ = 2
-const inbox = ()=>{ return process.argv[__++] }
-const outbox = (val) => console.log(val);
+let __ = 2;
+let inbox = () => {
+  let retval = process.argv[__++];
+  if (!retval) {
+    return null
+  } else {
+    return retval
+  }
+};
+let outbox = (val) => console.log(val);
 
 //
 // PROGRAM
 //
+
+// for (let c = 0; c < 2;  ) {
+//   let item = inbox()
+//   let secondItem = inbox()
+//   outbox(secondItem)
+//   outbox(item)
+// }
+
+while(hasInbox()) {
+  nextInbox = inbox()
+  if (nextInbox !== 'Eugene') {
+    outbox('Hello ' + nextInbox + '!')
+  } else {
+    outbox('Grrrrr...')
+  }
+}
+
+let nextInbox
+while(nextInbox !== null) do {
+  nextInbox = inbox()
+  if (nextInbox !== 'Eugene') {
+    outbox('Hello ' + nextInbox + '!')
+  } else {
+    outbox('Grrrrr...')
+  }
+}
