6184f313be869a8ee93422ca018d31c4a49b7747
Eugene Zaretskiy <ezaretskiy@attentivemobile.com>
2021-12-27 00:30:14 -0500

    Update bgg.mjs

--- src/bgg.mjs
+++ src/bgg.mjs
@@ -1,23 +1,41 @@
-import { getBggCollection, getBggThing } from 'bgg-xml-api-client';
+import { getBggCollection, getBggThing } from "bgg-xml-api-client";
 
-const number_of_players = process.argv[2]
+const number_of_players = process.argv[2];
 
 function getPrimaryName(item) {
-    if (Array.isArray(item.name)) {
-        return item.name.find(name => name.type === 'primary').value
-    } else {
-        return item.name.value
-    }
+  if (Array.isArray(item.name)) {
+    return item.name.find((name) => name.type === "primary").value;
+  } else {
+    return item.name.value;
+  }
 }
 
-async function run() {
-    const { data } = await getBggCollection({ username: 'ezaretskiy' });
+const { data } = await getBggCollection({ username: "ezaretskiy" });
 
-    const games = await getBggThing({ type: 'boardgame', id: data.item.map(thing => thing.objectid).join(',') })
-    const filteredGames = games.data.item.filter(item => item.maxplayers.value >= number_of_players && item.minplayers.value <= number_of_players)
-    const randomGame = filteredGames[Math.floor(Math.random() * (filteredGames.length))]
+let ids = "";
 
-    console.log(`Play: ${getPrimaryName(randomGame)}`)
+for (let i = 0; i < data.item.length; i++) {
+  ids = ids + data.item[i].objectid;
+  if (i !== data.item.length - 1) {
+    ids = ids + ",";
+  }
+}
+const games = await getBggThing({
+  type: "boardgame",
+  id: ids,
+});
+
+let filteredGames = [];
+for (let i = 0; i < games.data.item.length; i++) {
+  if (
+    games.data.item[i].maxplayers.value >= number_of_players &&
+    games.data.item[i].minplayers.value <= number_of_players
+  ) {
+    filteredGames.push(games.data.item[i]);
+  }
 }
 
-run().then(()=>{}).catch((e)=>console.error(e))
+const randomGame =
+  filteredGames[Math.floor(Math.random() * filteredGames.length)];
+
+console.log(`Play: ${getPrimaryName(randomGame)}`);
